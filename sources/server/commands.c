7457ca0d9a3762be76016a04248890d849edbed1
audibe_l <audibe_l@epitech.eu>
2015-05-07 16:40:39 +0200

    add pointeur sur fonction pour commands client server

---
+++ sources/server/commands.c
@@ -0,0 +1,17 @@
+/*
+** commands.c for zappy in /home/audibe_l/rendu/PSU_2014_zappy/sources/server
+** 
+** Made by Audibert Louis
+** Login   <audibe_l@epitech.net>
+** 
+** Started on  Thu May  7 16:30:08 2015 Audibert Louis
+** Last update Thu May  7 16:30:51 2015 Audibert Louis
+*/
+
+#include "../../headers/server.h"
+
+int	cmd_team(t_server *s)
+{
+  (void)s;
+  return (0);
+}
