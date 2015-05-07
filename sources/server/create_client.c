1af5255af2a3a99deef5824c5332ebce474c2490
Serge Heitzler <serge.heitzler@epitech.eu>
2015-05-07 15:15:40 +0200

    update prototype func server

--- sources/server/create_client.c
+++ sources/server/create_client.c
@@ -1,16 +1,16 @@
 /*
-** init_client.c for zappy in /home/heitzl_s/rendu/PSU_2014_zappy/sources/server
+** create_client.c for zappy in /home/heitzl_s/rendu/PSU_2014_zappy/sources/server
 **
 ** Made by Serge Heitzler
 ** Login   <heitzl_s@epitech.net>
 **
-** Started on  Thu May  7 10:41:09 2015 Serge Heitzler
-** Last update Thu May  7 10:55:17 2015 Serge Heitzler
+** Started on  Thu May  7 14:50:39 2015 Serge Heitzler
+** Last update Thu May  7 14:50:41 2015 Serge Heitzler
 */
 
 #include "../../headers/server.h"
 
-void		init_client(t_client *client, int fd,
+void		create_client(t_client *client, int fd,
 			    int nbr_playersn, char *team_name)
 {
   client->fd = fd;
