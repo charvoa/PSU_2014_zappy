2b1a080d48269340a0d8ed80476e72cf141f4910
audibe_l <audibe_l@epitech.eu>
2015-05-07 15:33:39 +0200

    add exec_cmd.c

--- sources/server/exec_cmd.c
+++ sources/server/exec_cmd.c
@@ -5,12 +5,16 @@
 ** Login   <audibe_l@epitech.net>
 ** 
 ** Started on  Thu May  7 15:24:45 2015 Audibert Louis
-** Last update Thu May  7 15:25:43 2015 Audibert Louis
+** Last update Thu May  7 15:33:22 2015 Audibert Louis
 */
 
 #include "../../headers/server.h"
 
 void	exec_cmd(t_server *s)
 {
-
+  int	(*cmd[12])(t_serv *s);
+  int	i;
+  
+  i = 0;
+  
 }
