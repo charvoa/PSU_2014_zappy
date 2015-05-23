9fe31f98b7d903f8e0e183d0264e118e4e8f0b11
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-05-23 17:44:06 +0200

    Parsing of connection

--- sources/client/InterpretClass.py
+++ sources/client/InterpretClass.py
@@ -2,16 +2,23 @@ class InterpretClass():
 
     """ A class that interpret messages received """
 
+    x = 0
+    y = 0
+    nbr_client = 0
+
     def interpret_bienvenue(self, s, rec, p):
         if (rec == b'BIENVENUE\r\n'):
-            if (p.getVerbose):
+            if (p.getVerbose()):
                 print(rec)
             return 1
+        return 0
 
     def interpret_num_client(self, s, rec, p):
-        print(rec)
-        print('NUMBER OF CLIENT')
+        if (p.getVerbose()):
+            print(rec)
+        nbr_client = int(rec)
 
     def interpret_size(self, s, rec, p):
-        print(rec)
-        print('SIZE')
+        if (p.getVerbose()):
+            print(rec)
+        x, y = rec.split(' - ')
