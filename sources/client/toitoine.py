dd2f35a409727f4e032c3b50e970817dc899db77
antgar <antoine.garcia@epitech.eu>
2015-05-24 09:25:59 +0200

    Add pose_object cmd

--- sources/client/toitoine.py
+++ sources/client/toitoine.py
@@ -5,3 +5,9 @@ class CommandToine:
             print(rec)
         var = 'prend object\n'
         mess.sendMessage(s, var)
+
+    def pose_objet(self, s, p, mess):
+        if (p.getVerbose()):
+            print(rec)
+        var = 'pose object\n'
+        mess.sendMessage(s, var)
