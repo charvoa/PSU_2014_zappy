7e925e9bf39f253255fec77862178028c99465a1
Serge Heitzler <serge.heitzler@epitech.eu>
2015-06-24 07:12:54 +0200

    minors update on cmd view

--- sources/server/cmd/determine_pos.c
+++ sources/server/cmd/determine_pos.c
@@ -5,38 +5,41 @@
 ** Login   <sergeheitzler@epitech.net>
 ** 
 ** Started on  Tue Jun 23 20:37:51 2015 Serge Heitzler
-** Last update Wed Jun 24 06:57:47 2015 Serge Heitzler
+** Last update Wed Jun 24 07:12:04 2015 Serge Heitzler
 */
 
 #include "server.h"
 
 int		determine_pos_x(t_client *c, t_size *size, int t, int l)
 {
-  (void)size;
   if (c->orientation == UP)
     return ((c->pos->x + t) >= 0 ?
-	    (c->pos->x + t) % size->width : (size->width - c->pos->x + t));
+	    (c->pos->x + t) % size->width : (size->width + c->pos->x + t));
   if (c->orientation == RIGHT)
     return ((c->pos->x + l) >= 0 ?
-	    (c->pos->x + l) % size->width : (size->width - c->pos->x + l));
+	    (c->pos->x + l) % size->width : (size->width + c->pos->x + l));
   if (c->orientation == DOWN)
     return ((c->pos->x - t) >= 0 ?
-	    (c->pos->x - t) % size->width : (size->width - c->pos->x - t));
+	    (c->pos->x - t) % size->width : (size->width + c->pos->x - t));
   if (c->orientation == LEFT)
-    return ((c->pos->x - l) % size->width);
+    return ((c->pos->x - l) >= 0 ?
+	    (c->pos->x - l) % size->width : (size->width + c->pos->x - l));
   return (SUCCESS);
 }
 
 int		determine_pos_y(t_client *c, t_size *size, int t, int l)
 {
-  (void)size;
   if (c->orientation == UP)
-    return ((c->pos->y + l) % size->height);
+    return ((c->pos->y + l) >= 0 ?
+	    (c->pos->y + l) % size->height : (size->height + c->pos->y + l));
   if (c->orientation == RIGHT)
-    return ((c->pos->y + t) % size->height);
+    return ((c->pos->y + t) >= 0 ?
+	    (c->pos->y + t) % size->height : (size->height + c->pos->y + t));
   if (c->orientation == DOWN)
-    return ((c->pos->y - l) % size->height);
+    return ((c->pos->y - l) >= 0 ?
+	    (c->pos->y - l) % size->height : (size->height + c->pos->y - l));
   if (c->orientation == LEFT)
-    return ((c->pos->y - t) % size->height);
+    return ((c->pos->y - t) >= 0 ?
+	    (c->pos->y - t) % size->height : (size->height + c->pos->y - t));
   return (SUCCESS);
 }
