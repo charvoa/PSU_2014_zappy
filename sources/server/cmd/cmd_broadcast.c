36b23810fc7bbb2020d5672e859909c5157e81c9
audibe_l <audibe_l@epitech.eu>
2015-06-25 14:36:26 +0200

    broadcast 100% functionnal

--- sources/server/cmd/cmd_broadcast.c
+++ sources/server/cmd/cmd_broadcast.c
@@ -5,25 +5,19 @@
 ** Login   <sergeheitzler@epitech.net>
 ** 
 ** Started on  Fri Jun 19 11:29:29 2015 Serge Heitzler
-** Last update Thu Jun 25 14:16:00 2015 Audibert Louis
+** Last update Thu Jun 25 14:35:23 2015 Audibert Louis
 */
 
 #include "server.h"
 
 char		*get_trame_broadcast(t_client *c, char *text)
 {
-  char		x[3];
-  char		y[3];
   char		*final;
   
-  sprintf(x, "%d", c->pos->x);
-  sprintf(y, "%d", c->pos->y);
-  final = xmalloc((strlen("message ") + strlen(x) + strlen(y)
-		   + strlen(text) + 2) * sizeof(char));
-  bzero(final, (strlen("message ") + strlen(x) + strlen(y)
-		+ strlen(text) + 2));
-  sprintf(final, "message %s %s,%s\n", x, y, text);
-  printf("final = %s\n", final);
+  final = xmalloc((strlen("message ") + strlen(text) + 8) * sizeof(char));
+  bzero(final, (strlen("message ") + strlen(text) + 8));
+  sprintf(final, "message %d %d,%s\n", c->pos->y, c->pos->x, text);
+  printf("final = %s", final);
   return (final);
 }
 
@@ -36,7 +30,7 @@ void		get_text(char *text, const char *cmd)
   j = 0;
   while (cmd[i])
     {
-      if (i >= 9)
+      if (i > 9)
 	{
 	  text[j] = cmd[i];
 	  j++;
@@ -58,8 +52,7 @@ int		cmd_broadcast(t_server *s, t_client *c, const char *cmd)
   text = xmalloc((strlen(cmd) - 9) * sizeof(char));
   bzero(text, strlen(cmd) - 9);
   get_text(text, cmd);
-  /* sscanf(cmd, "broadcast %s", text); */
-  printf("text = %s", text);
+  printf("text = %s\n", text);
   final = get_trame_broadcast(c, text);
   c_iterate = xmalloc(sizeof(t_client));
   c_iterate = tmp->data;
