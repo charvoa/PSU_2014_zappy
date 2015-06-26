64b2838cbf49af7d6a4ac115540071c0bf8f4c72
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-06-26 13:28:17 +0200

    IAClass ...

---
+++ sources/client/IAClass.py
@@ -0,0 +1,19 @@
+import time
+from CommandClass import *
+
+class IAClass():
+
+    """ IAClass that handle the zappy's AI """
+
+    def __ini__(self):
+        self.linemate = 0
+        self.deraumere = 0
+        self.sibur = 0
+        self.mendiane = 0
+        self.phiras = 0
+        self.thystame = 0
+        self.cc = CommandClass()
+
+    def run(self, s, p, mess):
+        while 1:
+            cc.voir_cmd(s, p, mess)
