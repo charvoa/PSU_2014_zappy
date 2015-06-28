f7760c8aebd7d01497f8c006dc2988b35cbbcc83
Serge Heitzler <serge.heitzler@epitech.eu>
2015-06-28 15:49:55 +0200

    add remove_client_by_id

--- sources/server/list/get_client_in_list.c
+++ sources/server/list/get_client_in_list.c
@@ -5,7 +5,7 @@
 ** Login   <sergeheitzler@epitech.net>
 ** 
 ** Started on  Fri Jun 19 11:30:14 2015 Serge Heitzler
-** Last update Sat Jun 27 17:39:36 2015 Serge Heitzler
+** Last update Sun Jun 28 15:48:50 2015 Serge Heitzler
 */
 
 #include "server.h"
@@ -27,4 +27,29 @@ t_client		*get_client_by_id(t_list *list, int id)
       tmp = tmp->next;
     }
   return (NULL);
+}
+
+int			remove_client_by_id(t_list *list, int id)
+{
+  t_node		*tmp;
+  t_client		*client;
+  int			i;
+
+  i = 0;
+  tmp = list->start;
+  client = xmalloc(sizeof(t_client));
+  client->inventory = xmalloc(sizeof(t_inventory));
+  client = tmp->data;
+  while (tmp)
+    {
+      client = tmp->data;
+      if (id == client->fd)
+	{
+	  remove_at_index(list, i);
+	  return (SUCCESS);
+	}
+      i++;
+      tmp = tmp->next;
+    }
+  return (ERROR);  
 }
