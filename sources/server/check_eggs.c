cd46e53ec8aeab40c42338ad0397bcc3755aabea
audibe_l <audibe_l@epitech.eu>
2015-07-03 15:59:06 +0200

    egg death modif

--- sources/server/check_eggs.c
+++ sources/server/check_eggs.c
@@ -5,7 +5,7 @@
 ** Login   <audibe_l@epitech.net>
 ** 
 ** Started on  Tue Jun 30 14:23:33 2015 Audibert Louis
-** Last update Thu Jul  2 11:13:59 2015 Audibert Louis
+** Last update Fri Jul  3 15:58:41 2015 Audibert Louis
 */
 
 #include "functions.h"
@@ -46,9 +46,11 @@ void		check_eggs(t_server *s)
 	{
 	  if (egg->fd != -1)
 	    create_client_from_egg(s, egg, i);
-	  else
-	    cmd_edi(egg, s->clients);
 	}
+      else if (egg->eclos.tv_sec > s->now.tv_sec ||
+	       ((egg->eclos.tv_sec > s->now.tv_sec) &&
+		(egg->eclos.tv_nsec > s->now.tv_nsec)))
+	cmd_edi(egg, s->clients);
       i++;
       tmp = tmp->next;
     }
