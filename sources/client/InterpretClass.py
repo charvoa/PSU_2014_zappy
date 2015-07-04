1dfb5ad34db205335287abcc042fd4ed70a456c8
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-07-04 19:42:03 +0200

    client qui marche

--- sources/client/InterpretClass.py
+++ sources/client/InterpretClass.py
@@ -4,9 +4,10 @@ class InterpretClass():
 
     """ A class that interpret messages received """
 
-    self.x = 0
-    self.y = 0
-    self.nbr_client = 0
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.nbr_client = 0
 
     def interpret_bienvenue(self, s, rec, p):
         if (p.getVerbose()):
